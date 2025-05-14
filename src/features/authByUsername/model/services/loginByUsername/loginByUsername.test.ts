6399efa7bbc37db2492f96b90ecc065d856930ac
Roman <kiseleow.roman2013@yandex.ru>
2025-05-14 09:59:24 +0300

    fix: tests

--- src/features/authByUsername/model/services/loginByUsername/loginByUsername.test.ts
+++ src/features/authByUsername/model/services/loginByUsername/loginByUsername.test.ts
@@ -1,11 +1,7 @@
-import axios from 'axios'
 import { loginByUsername } from "./loginByUsername"
 import { type StateSchema } from "@/app/providers/store"
 import { userActions } from "@/entities/user"
 import { TestAsyncThunk } from "@/shared/lib/test/testAsyncThunk"
-
-jest.mock('axios')
-const mockedAxios = jest.mocked(axios, true)
 
 describe('loginByUsername.test', () => {
     let state: DeepPartial<StateSchema>
@@ -18,20 +14,20 @@ describe('loginByUsername.test', () => {
 
     test('success request', async () => {
         const userValue = { username: '123', id: '1' }
-        mockedAxios.post.mockReturnValue(Promise.resolve({ data: userValue }))
         const thunk = new TestAsyncThunk(loginByUsername, state)
+        thunk.api.post.mockReturnValue(Promise.resolve({ data: userValue }))
         const result = await thunk.callThunk()
 
         expect(thunk.dispatch).toHaveBeenCalledWith(userActions.setAuthData(userValue))
         // eslint-disable-next-line @typescript-eslint/unbound-method
-        expect(mockedAxios.post).toHaveBeenCalled()
+        expect(thunk.api.post).toHaveBeenCalled()
         expect(result.meta.requestStatus).toBe('fulfilled')
         expect(result.payload).toEqual(userValue)
     })
 
     test('error request', async () => {
-        mockedAxios.post.mockReturnValue(Promise.resolve({ status: 403 }))
         const thunk = new TestAsyncThunk(loginByUsername, state)
+        thunk.api.post.mockReturnValue(Promise.resolve({ status: 403 }))
         const result = await thunk.callThunk()
 
         // eslint-disable-next-line @typescript-eslint/unbound-method
--- src/shared/lib/test/testAsyncThunk.ts
+++ src/shared/lib/test/testAsyncThunk.ts
@@ -1,5 +1,9 @@
 import { type AsyncThunkAction } from "@reduxjs/toolkit"
 import { type StateSchema } from "@/app/providers/store"
+import axios, { type AxiosStatic } from "axios"
+
+jest.mock('axios')
+const mockedAxios = jest.mocked(axios, true)
 
 type ActionCreatorType<Return, Arg, RejectedValue> =
     (arg: Arg) => AsyncThunkAction<Return, Arg, { rejectValue: RejectedValue }>
@@ -8,16 +12,23 @@ export class TestAsyncThunk<Return, Arg, RejectedValue> {
     dispatch: jest.MockedFn<any>
     getState: GetStateType
     actionCreator: ActionCreatorType<Return, Arg, RejectedValue>
+    api: jest.MockedFunctionDeep<AxiosStatic>
+    navigate: jest.MockedFn<any>
 
     constructor (actionCreator: ActionCreatorType<Return, Arg, RejectedValue>, state?: DeepPartial<StateSchema>) {
         this.actionCreator = actionCreator
         this.dispatch = jest.fn()
         this.getState = jest.fn(() => state || {})
+        this.api = mockedAxios
+        this.navigate = jest.fn()
     }
 
     async callThunk (arg: Arg) {
         const action = this.actionCreator(arg)
-        const result = await action(this.dispatch, this.getState, undefined)
+        const result = await action(this.dispatch, this.getState, {
+            api: this.api,
+            navigate: this.navigate
+        })
 
         return result
     }
